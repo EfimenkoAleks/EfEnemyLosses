bcd414742b3020bdd590039f6fd2fe5a8d505d34
gazservissana.zp@gmail.com <gazservissana.zp@gmail.com>
2023-08-26 16:56:13 +0300

    Initial Commit

---
+++ EfEnemyLosses/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EfEnemyLosses
+//
+//  Created by user on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
